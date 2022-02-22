a69bd6cc200392d883a8053b5fcb9fd4bb15546c
Claes Engelin <claes.engelin@zocom.se>
2022-02-22 15:38:29 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using DRYDemoLibrary;
+
+Console.Write("What is your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string lastName = Console.ReadLine();
+
+EmployeeProcessor processor = new EmployeeProcessor();
+string employeeID = processor.GenerateEmployeeID(firstName, lastName);
+
+Console.WriteLine($"Your employee ID is: {employeeID}");
+
+Console.ReadLine();
